09c80a4bab486a892116b6030a2146e7374ff791
Jordan Leslie <jordan02leslie@gmail.com>
2022-06-20 21:42:05 -0400

    test code for duplicates

--- src/AddTodo.test.js
+++ src/AddTodo.test.js
@@ -21,6 +21,19 @@ afterEach(() => {
 
  test('test that App component doesn\'t render dupicate Task', () => {
   render(<App />);
+  const inputTask = screen.getByRole('textbox', {name: /Add New Item/i});
+  const inputDate = screen.getByPlaceholderText("mm/dd/yyyy");
+  const element = screen.getByRole('button', {name: /Add/i});
+  const dueDate = "05/30/2023";
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  fireEvent.change(inputTask, { target: { value: "History Test"}});
+  fireEvent.change(inputDate, { target: { value: dueDate}});
+  fireEvent.click(element);
+  const check = screen.getAllByText(/History Test/i);
+  expect(check.length).toBe(1);
+
  });
 
  test('test that App component doesn\'t add a task without task name', () => {
